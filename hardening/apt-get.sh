3565d9f08aeb14aff235fefa1b1e9b8a49ec959a
nobodyCloak <53190047+nobodyCloak@users.noreply.github.com>
2020-02-03 12:20:23 -0700

    Update apt-get.sh

--- hardening/apt-get.sh
+++ hardening/apt-get.sh
@@ -4,20 +4,73 @@
 apt-get install lsb_release -y
 lsb_release -a | grep 'Codename' > codename.txt
 codename="$(cut -f2 -d$'\t' codename.txt)
-debian_repo="deb http://deb.debian.org/debian $codename main
+debian_repo=\"deb http://deb.debian.org/debian $codename main
 deb-src http://deb.debian.org/debian $codename main
 
 deb http://deb.debian.org/debian-security/ $codename/updates main
 deb-src http://deb.debian.org/debian-security/ $codename/updates main
 
 deb http://deb.debian.org/debian $codename-updates main
-deb-src http://deb.debian.org/debian $codename-updates main"
+deb-src http://deb.debian.org/debian $codename-updates main\""
+
+
+
+
+
+
+# empty space to help coordinate
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 echo $debian_repo | sudo tee /etc/apt/sources.list.d/master.list
 
-apt-get update
-
 # 2. Install nmap, htop, 7zip (for easy encryption of sensitive data)
+apt-get update
 apt-get install apt -y
 apt install nmap htop p7zip iptables -y
 
@@ -26,7 +79,6 @@ apt install nmap htop p7zip iptables -y
 
 apt update -y & nmap -p- 127.0.0.1 -oN open_ports.txt
 wait
-
 grep -A 200 PORT open_ports.txt | grep '\n' | grep -v Nmap | grep -o '[0-9]*' > open_port_numbers.txt
 cat open_ports.txt
 
@@ -71,27 +123,28 @@ while IFS= read -r users; do
     usermod -s /sbin/nologin $users
 done < users_list.txt
 
+for user in $(cut -f1 -d: /etc/passwd); do
+	echo $user
+	sudo crontab -u $user -l
+done
+
 rm users_list.txt
-
-
 apt autoremove -y
 
 curl -O rkhunter-1.4.6.tar.gz https://svwh.dl.sourceforge.net/project/rkhunter/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz
+wait
 tar -zxvf rkhunter-1.4.6.tar.gz
 sudo sh rkhunter-1.4.6/installer.sh --install
 sudo rkhunter -c --rwo --sk > rkhunter_results.txt
 cat rkhunter_results.txt
 
-# curl -O maldetect-current.tar.gz http://www.rfxn.com/downloads/maldetect-current.tar.gz
-# tar -zxvf maldetect-current.tar.gz
-# cd maldetect-*
-# sudo sh install.sh
 
+curl -O maldetect-current.tar.gz http://www.rfxn.com/downloads/maldetect-current.tar.gz
+wait
+tar -zxvf maldetect-current.tar.gz
+cd maldetect-*
+sudo sh install.sh
 
-
-for user in $(cut -f1 -d: /etc/passwd); do
-	echo $user
-	sudo crontab -u $user -l
-done
+maldetect-*/files/./maldetect
 
 

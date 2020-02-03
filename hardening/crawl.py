f18ad916df69e50b2645963047b2b9900031ab53
nobodyCloak <53190047+nobodyCloak@users.noreply.github.com>
2020-02-03 12:49:08 -0700

    Create crawl.py

---
+++ hardening/crawl.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+
+
+# def file_pathway(filename):
+
+print("Give the beginning pathway")
+file = input()
+print("Give output file: ")
+pathway = input()
+print("Is that correct? (y/n)")
+if input() != "y":
+    print("Nevermind...")
+else:
+    for root, dirs, files in os.walk(file, topdown=False):
+        for f in files:
+            try:
+                current_file = os.path.join(root, f)
+                final_path = str(Path(current_file).absolute())
+                print(final_path)
+            except FileNotFoundError as e:
+                # print("No such file or directory: ", e)
+                with open("pyScript_errors.txt", "a") as errorFile:
+                    errorFile.write("Possible erroneous alias/shortcut: " + str(e) + "\n\n")
+                continue
+            except PermissionError as f:
+                # print("Operation not permitted: ", f)
+                with open("pyScript_errors.txt", "a") as errorFile:
+                    errorFile.write("Are you root?" + str(f) + "\n\n")
+                continue
+            except OSError as o:
+                # print("Operation not permitted: ", o)
+                with open("pyScript_errors.txt", "a") as errorFile:
+                    errorFile.write("Check this out: " + str(o) + "\n\n")
+                continue
